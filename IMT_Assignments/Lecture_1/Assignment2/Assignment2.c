951f2c83cb7eca5e5795469f1b430d15b31dc7f9
Mohamed Hamdy <117542420+1MohamedHamdy@users.noreply.github.com>
2022-12-01 17:06:55 +0200

    Add files via upload

---
+++ IMT_Assignments/Lecture_1/Assignment2/Assignment2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(void)
+{
+	// variables for 3 numbers
+	int num1 , num2 , num3 ;
+	// scan first number from user
+	printf("Please enter number 1: ");
+	scanf("%d",&num1);
+	// scan second number from user
+	printf("Please enter number 2: ");
+	scanf("%d",&num2);
+	// scan third number from user
+	printf("Please enter number 3: ");
+	scanf("%d",&num3);
+	
+	//print numbers in reversed order
+	printf("number 3: %d\n",num3);
+	printf("number 2: %d\n",num2);
+	printf("number 1: %d",num1);
+	
+}
