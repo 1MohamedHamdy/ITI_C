9916a723f294c0ee883d2150c4cea047d7cc4205
Mohamed Hamdy <117542420+1MohamedHamdy@users.noreply.github.com>
2022-12-01 17:17:46 +0200

    Add files via upload

---
+++ IMT_Assignments/Lecture_2/Assignment1/Assignment1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(void)
+{
+	//in this lab we don't use loops yet
+	printf("     *     \n");
+	printf("    ***    \n");
+	printf("   *****   \n");
+	printf("  *******  \n");
+	printf(" ********* \n");
+	printf("***********\n");
+	
+}
