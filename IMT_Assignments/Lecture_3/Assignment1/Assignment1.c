912d137bb9cc4367e4334afc3f231255ba9faf3a
Mohamed Hamdy <117542420+1MohamedHamdy@users.noreply.github.com>
2022-12-01 17:29:16 +0200

    Add files via upload

---
+++ IMT_Assignments/Lecture_3/Assignment1/Assignment1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//Variables for 2 numbers 
+	int num1 , num2 ;
+	//scan 2 numbers from user and print their summation in while loop to prevent program to end until user close window
+	while(1){
+		printf("Plese enter first number ");
+		scanf("%d",&num1);
+		printf("Plese enter second number ");
+		scanf("%d",&num2);
+		printf("The result is %d\n",num1+num2);
+		printf("\n");
+		
+	}
+	
+	
+}
